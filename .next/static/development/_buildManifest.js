31534709d301ad1cc3fdf6d6138b6f9d85cbe9c2
hungvotan <tanhung2211@gmail.com>
2021-06-22 20:56:38 +0700

    remove

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/lister/[pid]":["static\u002Fchunks\u002Fpages\u002Flister\u002F[pid].js"],sortedPages:["\u002F_app","\u002F_error","\u002Flister\u002F[pid]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
